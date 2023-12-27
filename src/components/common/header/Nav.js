e4913be967176c9618b72b40166ac4ee1a3735d0
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 21:35:19 +0000

    Add styles to  navbar

--- src/components/common/header/Nav.js
+++ src/components/common/header/Nav.js
@@ -1,13 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import { Menu } from './index';
+
+const NavBarWrapper = styled.nav`
+    background: #B39DDB;
+    padding: 10px 20px;
+    color: #212121;
+`;
 
 class NavBar extends React.Component {
     render () {
         const { items } = this.props;
         return (
-            <nav>
+            <NavBarWrapper>
                 <Menu items={items} />
-            </nav>
+            </NavBarWrapper>
         );
     }
 }
