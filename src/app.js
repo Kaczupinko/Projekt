00ea4bdbf451c79d85ca4d767dc76c96400121fc
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 16:42:27 +0000

    Add basic app.js file

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import StarterPage from "./pages/StarterPage";
+
+const root = createRoot(document.querySelector('#root'));
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <StarterPage />
+            </Router>
+        )
+    }
+}
+
+root.render(<App />);
