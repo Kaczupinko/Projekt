88d7b0d0d8679c901ab5cf66623930fc1821f866
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 16:45:42 +0000

    Create FeatureSection

---
+++ src/pages/starter-components/FeaturesSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class FeaturesSection extends React.Component {
+
+    render() {
+        return (
+            <section>
+                <h1>Features Section</h1>
+            </section>
+        )
+    }
+}
+
+export default FeaturesSection;
