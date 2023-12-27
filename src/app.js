5badcdd77c49dc3a233f284a319bd1964d2c887c
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 21:34:01 +0000

    Import css to app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import StarterPage from "./pages/StarterPage";
+import "./app.css";
 
 const root = createRoot(document.querySelector('#root'));
 
