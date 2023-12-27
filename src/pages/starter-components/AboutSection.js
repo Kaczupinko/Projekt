4ef2b6c0fc17347ba0b14cccdd053f929cf7a629
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 16:45:08 +0000

    Create basic AboutSection

---
+++ src/pages/starter-components/AboutSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutSection extends React.Component {
+    
+    render() {
+        return (
+            <section>
+                <h1>About Section</h1>
+            </section>
+        )
+    }
+}
+
+export default AboutSection;
