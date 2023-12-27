8a55e0a991ec2c12d3f27e447932172c340f0544
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 16:46:34 +0000

    Create index for export components in starter page

---
+++ src/pages/starter-components/index.js
@@ -0,0 +1,13 @@
+import AboutSection from "./AboutSection";
+import CallToAction from "./CallToAction";
+import HeroSection from "./HeroSection";
+import TestimonialsSection from "./TestimonialsSection";
+import FeaturesSection from "./FeaturesSection";
+
+export {
+    AboutSection,
+    CallToAction,
+    HeroSection,
+    TestimonialsSection,
+    FeaturesSection
+};
