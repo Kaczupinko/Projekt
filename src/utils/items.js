074fb78d1369c4b2b6718ee6c49879a27cd983c5
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 16:46:12 +0000

    Create items file with information about starter page

---
+++ src/utils/items.js
@@ -0,0 +1,9 @@
+const items = [
+    { text: "Home", url: "/" },
+    { text: "Courses", url: "/courses" },
+    { text: "About", url: "/about" },
+    { text: "Contact", url: "/contact" },
+    { text: "Support", url: "/support" },
+];
+
+export default items;
