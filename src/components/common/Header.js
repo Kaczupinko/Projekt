908577efa73ac9e8ef9597465b767a48b1bfe958
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 21:34:23 +0000

    Add styles to header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,15 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 import { AuthButton, Logo, Nav } from "./header/index";
 import items from "./../../utils/items";
+
+const HeaderWrapper = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    background-color: #6200EA;
+    color: #fff;
+    font-family: "Roboto", sans-serif;
+`;
 
 class Header extends React.Component {
 
     render() {
         return (
-            <header>
+            <HeaderWrapper>
                 <Logo />
                 <Nav items={items} />
-            </header>
+            </HeaderWrapper>
         )
     }
 }
