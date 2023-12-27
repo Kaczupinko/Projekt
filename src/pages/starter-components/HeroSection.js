5bcee4460f8cfceeb534d42b514bcba14066eb0f
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 16:45:31 +0000

    Create basic HeroSection

---
+++ src/pages/starter-components/HeroSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class HeroSection extends React.Component {
+
+    render() {
+        return (
+            <section>
+                <h1>Hero Section</h1>
+            </section>
+        )
+    }
+}
+
+export default HeroSection;
