917ce62338e1a6d381d9b131a05d529b36bb6622
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 21:34:28 +0000

    Add styles to logo

--- src/components/common/header/Logo.js
+++ src/components/common/header/Logo.js
@@ -1,12 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import LogoImage from '../../../assets/images/logos/fox-Logo.png';
+
+const LogoWrapper = styled.div`
+    text-align: center;
+`;
+
+const StyledLogo = styled.img`
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    object-fit: cover;
+`;
 
 class Logo extends React.Component {
     
     render() {
         return (
-            <div className="logo">
-                <h1>Logo</h1>
-            </div>
+            <LogoWrapper>
+                <StyledLogo src={LogoImage} alt="Fox Logo" />
+            </LogoWrapper>
         )
     }
 }
