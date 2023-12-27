aed1fd22fad55ace7c3df6590ab2ae6c31db2e6c
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 21:35:04 +0000

    Add styles to menu

--- src/components/common/header/Menu.js
+++ src/components/common/header/Menu.js
@@ -1,15 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 import { MenuItem } from "./index";
+
+const MenuList = styled.ul`
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+`;
 
 class Menu extends React.Component {
     render() {
         const { items } = this.props;
         return (
-            <ul>
+            <MenuList>
                 {items.map((item, index) => {
                     return <MenuItem key={index} text={item.text} url={item.url} />
                 })}
-            </ul>
+            </MenuList>
         );
     }
 }
