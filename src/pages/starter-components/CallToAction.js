4d4dafc130d6e2262a5355727aa6841e52af8930
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 16:45:21 +0000

    Create basic CallToAction section

---
+++ src/pages/starter-components/CallToAction.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class CallToAction extends React.Component {
+
+    render() {
+        return (
+            <section>
+                <h1>Call To Action</h1>
+            </section>
+        )
+    }
+}
+
+export default CallToAction;
