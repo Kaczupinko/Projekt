4687f54087de91e72a7b272eb9babd6cfbba60a2
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 16:45:54 +0000

    Create basic TestimonialsSection

---
+++ src/pages/starter-components/TestimonialsSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TestimonialsSection extends React.Component {
+
+    render() {
+        return (
+            <section>
+                <h1>Testimonials Section</h1>
+            </section>
+        )
+    }
+}
+
+export default TestimonialsSection;
