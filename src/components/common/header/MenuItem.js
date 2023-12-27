3533dad6c2572c0da0095624d45b243505848ada
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 21:35:12 +0000

    Add styles to menuitem

--- src/components/common/header/MenuItem.js
+++ src/components/common/header/MenuItem.js
@@ -1,13 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
+
+const MenuItemWrapper = styled.li`
+    margin: 0 1em;
+`;
+
+const StyledLink = styled(Link)`
+    /* Add your styles for the link here */
+    color: #000;
+    text-decoration: none;
+    &:hover {
+        color: blue;
+    }
+`;
 
 class MenuItem extends React.Component {
     render() {
         const { text, url } = this.props;
         return (
-            <li>
-                <Link to={url}>{text}</Link>
-            </li>
+            <MenuItemWrapper>
+                <StyledLink to={url}>{text}</StyledLink>
+            </MenuItemWrapper>
         );
     }
 }
