1063a181630df4b01895cd451749944ab6de504c
Kaczupinko <kaczupinko@gmail.com>
2023-12-27 16:44:04 +0000

    Add index on Header components

---
+++ src/components/common/header/index.js
@@ -0,0 +1,13 @@
+import AuthButton from "./AuthButton";
+import Logo from "./Logo";
+import MenuItem from "./MenuItem";
+import Menu from "./Menu";
+import Nav from "./Nav";
+
+export {
+    AuthButton,
+    Logo,
+    MenuItem,
+    Menu,
+    Nav,
+};
